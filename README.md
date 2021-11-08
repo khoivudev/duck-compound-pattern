4007bd19ea5631535e1daadb906c19dc0d9c1903
khoivudev <vuhuykhoi1997@gmail.com>
2021-11-08 22:06:03 +0900

    [feat] implement quack counter decorator

---
+++ README.md
@@ -0,0 +1,4 @@
+1. Interface
+2. Adapter
+3. Decorator
+4. Factory
--- src/DuckSimulator.java
+++ src/DuckSimulator.java
@@ -5,17 +5,21 @@ public class DuckSimulator {
     }
 
     void simulate() {
-        Quackable mallarDuck = new MallardDuck();
-        Quackable redheadDuck = new RedheadDuck();
-        Quackable duckCall = new DuckCall();
-        Quackable rubberDuck = new RubberDuck();
+        Quackable mallarDuck = new QuackCounter(new MallardDuck());
+        Quackable redheadDuck = new QuackCounter(new RedheadDuck());
+        Quackable duckCall = new QuackCounter(new DuckCall());
+        Quackable rubberDuck = new QuackCounter(new RubberDuck());
+        Quackable gooseDuck = new QuackCounter(new GooseAdapter(new Goose()));
 
-        System.out.println("\n Duck Simulator");
+        System.out.println("\nDuck Simulator: With Goose Adapter");
 
         simulate(mallarDuck);
         simulate(redheadDuck);
         simulate(duckCall);
         simulate(rubberDuck);
+        simulate(gooseDuck);
+
+        System.out.println("The ducks quacked " + QuackCounter.getQuacks() + " times");
     }
 
     void simulate(Quackable duck) {
