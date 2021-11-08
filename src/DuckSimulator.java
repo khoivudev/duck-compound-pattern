6c5e4810ceee30621e9e904796c73063134b3e67
khoivudev <vuhuykhoi1997@gmail.com>
2021-11-08 21:52:00 +0900

    [feat] Implement Quackable interface

---
+++ src/DuckSimulator.java
@@ -0,0 +1,24 @@
+public class DuckSimulator {
+    public static void main(String[] args) {
+        DuckSimulator simulator = new DuckSimulator();
+        simulator.simulate();
+    }
+
+    void simulate() {
+        Quackable mallarDuck = new MallardDuck();
+        Quackable redheadDuck = new RedheadDuck();
+        Quackable duckCall = new DuckCall();
+        Quackable rubberDuck = new RubberDuck();
+
+        System.out.println("\n Duck Simulator");
+
+        simulate(mallarDuck);
+        simulate(redheadDuck);
+        simulate(duckCall);
+        simulate(rubberDuck);
+    }
+
+    void simulate(Quackable duck) {
+        duck.quack();
+    }
+}
